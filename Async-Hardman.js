6ddd97186828cb2c8209dee76c59fc95385da9b9
ruanxiaowu <2728795232@qq.com>
2025-09-22 17:48:38 +0800

    feat: 完善Hardman练习

---
+++ Async-Hardman.js
@@ -0,0 +1,56 @@
+class Hardman {
+    constructor(name) {
+        this.name = name
+        this.tasks = []
+
+        this.tasks.push(() => {
+            console.log(`${name} comes to school`)
+        })
+
+        setTimeout(() => {
+            this.handleTasks()
+        }, 0)
+    }
+
+    async handleTasks() {
+        for (let task of this.tasks) {
+            await task()
+        }
+    }
+
+    rest(seconds) {
+        this.tasks.push(async () => {
+            setTimeout(() => {
+                resolve()
+                console.log(`${this.name} rest for ${seconds} seconds`)
+            }, seconds * 1000)
+        })
+
+        return this
+    }
+
+    restFirst(seconds) {
+        this.tasks.unshift(async () => {
+            setTimeout(() => {
+                resolve()
+                console.log(`${this.name} restFirst for ${seconds} seconds`)
+            }, seconds * 1000)
+        })
+
+        return this
+    }
+
+    learn(subject) {
+        this.tasks.push(() => {
+            console.log(`${this.name} is learning ${subject}`)
+            return Promise.resolve() // 保持一致性
+        })
+        return this
+    }
+}
+
+const Jack = new Hardman('Jack')
+// console.log(Jack)
+
+// Jack.rest(5).learn('Chinese')
+Jack.restFirst(2).rest(3).learn('Math')
