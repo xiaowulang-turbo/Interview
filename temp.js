886d008f2682bf071735ebbeabf5dde7717e165f
ruanxiaowu <2728795232@qq.com>
2025-09-22 16:54:54 +0800

    feat: 添加防抖节流方法

--- temp.js
+++ temp.js
@@ -1,29 +1,54 @@
 // 防抖
 function debounce(fn, delay) {
-  let timer = null
+    let timer = null
 
-  return function (...args) {
-    if (timer) {
-      clearTimeout(timer)
+    return function (...args) {
+        if (timer) {
+            clearTimeout(timer)
+        }
+
+        timer = setTimeout(() => {
+            fn.apply(null, args)
+            timer = null
+        }, delay)
     }
-
-    timer = setTimeout(() => {
-      fn.apply(null, args)
-      timer = null
-    }, delay)
-  }
 }
 
 // 节流
 function throttle(fn, delay) {
-  let timer = null
+    let timer = null
 
-  return function (...args) {
-    if (!timer) {
-      timer = setTimeout(() => {
-        fn.apply(null, args)
-        timer = null
-      }, delay)
+    return function (...args) {
+        if (!timer) {
+            timer = setTimeout(() => {
+                fn.apply(null, args)
+                timer = null
+            }, delay)
+        }
     }
-  }
+}
+
+function debounceThrottle(fn, delay) {
+    let lastCall = 0
+    let timer = null
+
+    return function (...args) {
+        const now = Date.now()
+        const timeSinceLastCall = now - lastCall
+
+        if (timer) {
+            clearTimeout(timer)
+        }
+
+        if (timeSinceLastCall >= delay) {
+            fn.apply(this, args)
+            lastCall = Date.now()
+        } else {
+            timer = setTimeout(() => {
+                fn.apply(this, args)
+                lastCall = Date.now()
+                timer = null
+            }, delay)
+        }
+    }
 }
